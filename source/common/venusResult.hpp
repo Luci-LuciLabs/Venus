060570e5d0ed472ac12c4ed162a0efd1f5afe2d9
Luci Raine <luci.lucilabs@gmail.com>
2025-06-01 04:20:50 -0500

    Basic common headers, likely to be changed in future.

---
+++ source/common/venusResult.hpp
@@ -0,0 +1,16 @@
+#ifndef VENUS_RESULT_HPP
+#define VENUS_RESULT_HPP
+
+#include <cstdint>
+
+namespace venus{
+  
+  enum class VENUS_RESULT : int16_t{
+    VENUS_SUCCESS = 0,
+    VENUS_FAILURE = 1,
+
+  };
+
+  
+} // namespace venus
+#endif // VENUS_RESULT_HPP
