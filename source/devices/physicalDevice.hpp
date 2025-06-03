3ce96c45ba90c49da6ba4b8c8ec34eedb0fc4e23
Luci Raine <luci.lucilabs@gmail.com>
2025-06-03 14:23:37 -0500

    Removed unused physicalDevice member, git-hooks setup script has been completed.

--- source/devices/physicalDevice.hpp
+++ source/devices/physicalDevice.hpp
@@ -4,44 +4,39 @@
 #include "volk.h"
 #include "gpuStructs.hpp"
 
+namespace venus {
+	const std::vector<const char *> DEVICE_EXTENSIONS = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+	class PhysicalDevice {
+	public:
+		PhysicalDevice(const VkInstance &instanceRef, const VkSurfaceKHR &surfaceRef);
+		~PhysicalDevice();
 
+		// class should never be copied.
+		PhysicalDevice(const PhysicalDevice &) = delete;
+		PhysicalDevice &operator=(const PhysicalDevice &) = delete;
+		// class should never be moved.
+		PhysicalDevice(const PhysicalDevice &&) = delete;
+		PhysicalDevice &operator=(const PhysicalDevice &&) = delete;
 
-namespace venus{
-  const std::vector<const char *> DEVICE_EXTENSIONS = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
-  class PhysicalDevice{
-    public:
-      PhysicalDevice(const VkInstance& instanceRef, const VkSurfaceKHR& surfaceRef);
-      ~PhysicalDevice();
+		VkPhysicalDevice getDevice(void) const { return m_GPU_device; }
 
-      // class should never be copied.
-      PhysicalDevice(const PhysicalDevice&) = delete;
-      PhysicalDevice &operator=(const PhysicalDevice&) = delete;
-      // class should never be moved.
-      PhysicalDevice(const PhysicalDevice&&) = delete;
-      PhysicalDevice &operator=(const PhysicalDevice&&) = delete;
+	private:
+		VkInstance m_instance;
+		VkSurfaceKHR m_surface;
+		VkPhysicalDevice m_GPU_device = VK_NULL_HANDLE;
 
+		void chooseDevice(void);
+		bool deviceMeetsMinimumRequirements(VkPhysicalDevice device);
+		bool areDeviceExtensionsSupported(VkPhysicalDevice device);
+		uint32_t giveDeviceQualityRating(VkPhysicalDevice device);
 
-      VkPhysicalDevice getDevice(void) const {return m_GPU_device;}
-    private:
-      VkInstance m_instance;
-      VkSurfaceKHR m_surface;
-      VkPhysicalDevice m_GPU_device = VK_NULL_HANDLE;
-      uint32_t m_API_version = 0;
+		SwapchainSupportDetails querySwapchainSupportDetails(VkPhysicalDevice device);
+		QueueFamilyIndices findQueueFamilyIndices(VkPhysicalDevice device);
 
+		// queries and stores device properties, features, swapchainsupportdetails, queuefamilyindices, etc.
+		void storeDeviceCapabilites(void);
+	};
 
-      void chooseDevice(void);
-      bool deviceMeetsMinimumRequirements(VkPhysicalDevice device);
-      bool areDeviceExtensionsSupported(VkPhysicalDevice device);
-      uint32_t giveDeviceQualityRating(VkPhysicalDevice device);
-      
-      SwapchainSupportDetails querySwapchainSupportDetails(VkPhysicalDevice device);
-      QueueFamilyIndices findQueueFamilyIndices(VkPhysicalDevice device);
+}  // namespace venus
 
-      // queries and stores device properties, features, swapchainsupportdetails, queuefamilyindices, etc.
-      void storeDeviceCapabilites(void);
-      
-  };
-
-} // namespace venus
-
-#endif // VENUS_PHYSICAL_DEVICE_HPP
+#endif  // VENUS_PHYSICAL_DEVICE_HPP
