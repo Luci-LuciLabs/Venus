bbb408b43bc6b90c034a55652cedcedae215f61f
Luci Raine <luci.lucilabs@gmail.com>
2025-06-07 10:16:18 -0500

    Rewrote client main to populate appDetails and   construct the Application class with it.

--- source/.client/main.cpp
+++ source/.client/main.cpp
@@ -1,15 +1,26 @@
+#include "appDetails.hpp"
 #include "application.hpp"
 
-#include <stdexcept>
 #include <iostream>
+#include <memory>
 
-int main(void) {
-	venus::Application app;
+#include <print>
+
+auto main() -> int {
+	venus::AppDetails details = {.applicationName = "V_client",
+															 .windowTitle = "Venus Client",
+															 .defaultWindowResolution = venus::USE_720p_RES_BIT,
+															 .versionMajor = 1,
+															 .versionMinor = 0,
+															 .versionPatch = 0};
+
+	std::unique_ptr<venus::Application> VNS_APP = std::make_unique<venus::Application>(details);
 
 	try {
-		app.run();
+		VNS_APP->run();
 	} catch(const std::exception &e) {
 		std::cerr << e.what() << '\n';
+		return 1;
 	}
 
 	return 0;
