58fb5629db25f05820cea09e9c2652e5f88bde95
Luci Raine <luci.lucilabs@gmail.com>
2025-06-07 10:19:11 -0500

    Added comment detailing usage and false clangd   warnings.

--- source/logger/quill_PCH.hpp
+++ source/logger/quill_PCH.hpp
@@ -3,11 +3,15 @@
 
 #include "quill/Backend.h"
 #include "quill/Frontend.h"
-#include "quill/sinks/ConsoleSink.h"
-#include "quill/sinks/RotatingSink.h"
-#include "quill/sinks/RotatingJsonFileSink.h"
-#include "quill/Logger.h"
-#include "quill/LogMacros.h"
 #include "quill/LogFunctions.h"
+#include "quill/LogMacros.h"
+#include "quill/Logger.h"
+#include "quill/sinks/ConsoleSink.h"
+#include "quill/sinks/RotatingJsonFileSink.h"
+#include "quill/sinks/RotatingSink.h"
 
-#endif // VENUS_QUILL_PRECOMPILED_HPP
+// If you are using Clangd you will see several "unused-includes" warnings.
+// These are false warnings as this is a pre-compiled-header file to cut compile time.
+// You should ignore these warnings, DO NOT directly include these files into "VN_logger.cpp" OR "VN_logger.hpp".
+
+#endif  // VENUS_QUILL_PRECOMPILED_HPP
