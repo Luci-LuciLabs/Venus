08e17fa69295ac3dec9a2e09bc33722f9937f9b2
Luci Raine <luci.lucilabs@gmail.com>
2025-06-07 10:01:53 -0500

    Redefined WindowResolution enum as bitmask enum.   Updated version names to comply with suggestions   for american english.

--- source/common/appDetails.hpp
+++ source/common/appDetails.hpp
@@ -1,20 +1,32 @@
 #ifndef VENUS_APP_DETAILS_HPP
 #define VENUS_APP_DETAILS_HPP
 
+// STDLIB
+#include <cstdint>
 namespace venus {
 
-	struct AppDetails {
-		const char *name;
-		int versionMajour;
-		int versionMinour;
-		int versionPatch;
+	/**
+   * @brief Bit flags to set default window resolution, the default setting does not change resizeability.
+   * 
+   */
+	enum WindowResolution_BIT : uint32_t {  // TODO: SUPPORT FULLSCREEN
+		USE_480p_RES_BIT = (640 << 16) | 480,
+		USE_720p_RES_BIT = (1280 << 16) | 720,
+		USE_1080p_RES_BIT = (1920 << 16) | 1080,
 	};
 
-  struct WindowDetails {
-    const char* title;
-    int width;
-    int height;
-  };
+	/**
+   * @brief Used to supply Venus application-layer with basic details about the client-application.
+   * 
+   */
+	struct AppDetails {
+		const char *applicationName;
+		const char *windowTitle;
+		WindowResolution_BIT defaultWindowResolution;
+		unsigned int versionMajor;
+		unsigned int versionMinor;
+		unsigned int versionPatch;
+	};
 
 }  // namespace venus
 
